13f667c48dacbddae729ebbc4a9906f692cedfe0
Tobias Möller <35395808+tobiasmoeller@users.noreply.github.com>
2018-01-13 16:18:26 +0100

    styling changes

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,4 @@
 //This function adjusts the margin based on the height of object above to avoid overlapping elements
-function carouselMargin(id1,id2) {
+function topMargin(id1,id2) {
   document.getElementById(id1).style.marginTop = "" + document.getElementById(id2).offsetHeight + "px";
 }
